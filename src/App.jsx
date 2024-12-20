0fe64b18fb40808f398b8785696f00c401116086
Justin Fowler <justinfowlerart@gmail.com>
2024-12-19 19:37:27 -0500

    Add pokemon dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,39 @@ const endpoint = 'https://pokeapi.co/api/v2/pokemon/';
 
 function App() {
 	const [skip, setSkip] = useState(true);
-	const [name, setName] = useState('ditto');
+	const [name, setName] = useState('charmander');
 	const { data, error, loading } = useFetch(endpoint + name, skip);
 
 	useEffect(() => {
 		setSkip(false);
 	});
 
+	function onChange(e) {
+		setName(e.target.value);
+	}
+
 	return (
 		<>
-			{loading ? (
-				<h2>Loading...</h2>
-			) : error ? (
-				<h2>Error loading data</h2>
-			) : data ? (
-				<div>
-					<h3>{data.name}</h3>
-					<img src={data.sprites.front_default} />
-				</div>
-			) : (
-				<></>
-			)}
+			<select onChange={onChange}>
+				<option value='charmander'>charmander</option>
+				<option value='squirtle'>squirtle</option>
+				<option value='bulbasaur'>bulbasaur</option>
+				<option value='pidgey'>pidgey</option>
+			</select>
+			<div>
+				{loading ? (
+					<h2>Loading...</h2>
+				) : error ? (
+					<h2>Error loading data</h2>
+				) : data ? (
+					<div>
+						<h3>{data.name}</h3>
+						<img src={data.sprites.front_default} />
+					</div>
+				) : (
+					<></>
+				)}
+			</div>
 		</>
 	);
 }
